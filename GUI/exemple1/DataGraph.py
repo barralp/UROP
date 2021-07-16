3e2e89c16c2bf474d3304441b260360d7381d373
Julia-Pockat <julia.pockat@gmail.com>
2021-07-15 22:30:23 -0500

    attempt to fix the module problem

--- GUI/exemple1/DataGraph.py
+++ GUI/exemple1/DataGraph.py
@@ -1,23 +1,24 @@
-from database.communicate_database import getAMDuration, getAMFreq, getASPower, getASPowermW, getASPowermW2, getBigZ, getBlinckingDuration, getBlinckingFreq, getBlinckingLength, getBlinckingTime, getCameraFudgeTime, getCompHoldTime, getCompLevel, getCompLevel2, getCompTime, getCompTime2, getCompX, getCompXEarth, getCompY, getCompYEarth, getCompZ, getCompZ2, getCompZEarth, getDopplerCoolFreq, getDummy, getEvap1CompZ, getEvap1EndGradient, getEvap2Factor, getEvap5BigZ, getEvapEndGradient, getEvapGradient1, getEvapGradient2, getEvapGradient3, getEvapGradientInit, getEvapScan, getEvapTime1, getEvapTime2, getEvapTime3, getEvapTime4, getEvapTime5, getEvapTime6, getFeshbachCurrent, getFinalBField, getFinalODT1, getFinalYComp, getFreq1, getFreq2, getFreq3, getFreq4, getFreq5, getFreqCompTime2, getImageTime, getImgFreq, getInTrapCoolFreq, getInTrapCoolTime, getIodineFreq, getIterationCount, getIterationNum, getLevel1, getLevel2, getLevel3, getLevel4, getLevel5, getLoadCurrent, getLoadTime, getLossTime, getMOTCompFreq, getMOTCompFreq2, getMOTCurrentAmps, getMOTLevel, getMOTLoadCurrentAmps, getMolassesLevel, getMotLoadFreq, getODT1Evap1End, getODT1Final, getODT1Init, getODT2Evap1End, getODT2Final, getODT2Init, getODTCompX, getODTCompY, getODTCompZ, getODTHoldTime, getODTLoad_MOTFreq, getODTLoad_MOT_Freq, getODTMolasses, getODTMolassesZField, getODTRamp, getODTRampUp, getODTTOFBigZ, getODTTOFGradient, getPumpTime, getPumpingFreq, getRunID, getRunningCounter, getSGOn, getSGOn2, getTCFreq, getTOF, getTau, getTime, getTimeStamp, getTotalExp, getVar60, getWaitTime, getWee, getZSPower
 import sys
+sys.path.append("../UROP/database")
+from database.communicate_database import getAMDuration, getAMFreq, getASPower, getASPowermW, getASPowermW2, getBigZ, getBlinckingDuration, getBlinckingFreq, getBlinckingLength, getBlinckingTime, getCameraFudgeTime, getCompHoldTime, getCompLevel, getCompLevel2, getCompTime, getCompTime2, getCompX, getCompXEarth, getCompY, getCompYEarth, getCompZ, getCompZ2, getCompZEarth, getDopplerCoolFreq, getDummy, getEvap1CompZ, getEvap1EndGradient, getEvap2Factor, getEvap5BigZ, getEvapEndGradient, getEvapGradient1, getEvapGradient2, getEvapGradient3, getEvapGradientInit, getEvapScan, getEvapTime1, getEvapTime2, getEvapTime3, getEvapTime4, getEvapTime5, getEvapTime6, getFeshbachCurrent, getFinalBField, getFinalODT1, getFinalYComp, getFreq1, getFreq2, getFreq3, getFreq4, getFreq5, getFreqCompTime2, getImageTime, getImgFreq, getInTrapCoolFreq, getInTrapCoolTime, getIodineFreq, getIterationCount, getIterationNum, getLevel1, getLevel2, getLevel3, getLevel4, getLevel5, getLoadCurrent, getLoadTime, getLossTime, getMOTCompFreq, getMOTCompFreq2, getMOTCurrentAmps, getMOTLevel, getMOTLoadCurrentAmps, getMolassesLevel, getMotLoadFreq, getODT1Evap1End, getODT1Final, getODT1Init, getODT2Evap1End, getODT2Final, getODT2Init, getODTCompX, getODTCompY, getODTCompZ, getODTHoldTime, getODTLoad_MOTFreq, getODTLoad_MOT_Freq, getODTMolasses, getODTMolassesZField, getODTRamp, getODTRampUp, getODTTOFBigZ, getODTTOFGradient, getPumpTime, getPumpingFreq, getRunID, getRunningCounter, getSGOn, getSGOn2, getTCFreq, getTOF, getTau, getTime, getTimeStamp, getTotalExp, getVar60, getWaitTime, getWee, getZSPower
 import wx
 import numpy as np
 import matplotlib.pyplot as plt
 from ipywidgets import interact, interactive, fixed, interact_manual
 import ipywidgets as widgets
 
-def f(x) :
-    return x
-def g(y) :
-    return y
-
 class DataGraph(wx.Frame) :
     def __init__(self, parent):
         self.app = wx.App()
         super(DataGraph, self).__init__(parent, size=(450, 300))
-        # m_x = x
-        # m_y = y
-    def parseFile(self, selection) :
+    
+    def f(x) :
+        return x
+
+    def g(y) :
+        return y
+
+    def ParseFile(self, selection) :
         data = []
         if (selection == 'Run ID') :
             data = getRunID()
@@ -240,7 +241,7 @@ class DataGraph(wx.Frame) :
 
         
     def makeGraph(self) :
-        interact(f, x=['Run ID', 'Iteration Num', 'Iteration Count', 'Running Counter',
+        interact(self.f, x=['Run ID', 'Iteration Num', 'Iteration Count', 'Running Counter',
             'TOF', 'Comp Level', 'Img Freq', 'Dummy', 'Iodine Freq', 'Final B Field',
             'Camera Fudge Time', 'Load Time', 'Comp Time', 'Load Current', 'Time Stamp', 
             'Wee', 'Mot Load Freq', 'Mot Comp Freq', 'Time', 'ZS Power', 'Image Time', 
@@ -260,7 +261,7 @@ class DataGraph(wx.Frame) :
             'ODT Comp X', 'ODT Comp Y', 'ODT Comp Z', 'Blincking Length', 'ODT Load MOT Freq', 
             'Evap 1 Comp Z', 'Pumping Freq', 'Comp X Earth', 'Comp Y Earth', 'Comp Z Earth',
             'Evap Time 6', 'Comp Z 2'])
-        interact(g, y=['Run ID', 'Iteration Num', 'Iteration Count', 'Running Counter', 
+        interact(self.g, y=['Run ID', 'Iteration Num', 'Iteration Count', 'Running Counter', 
             'TOF', 'Comp Level', 'Img Freq', 'Dummy', 'Iodine Freq', 'Final B Field',
             'Camera Fudge Time', 'Load Time', 'Comp Time', 'Load Current', 'Time Stamp',
             'Wee', 'Mot Load Freq', 'Mot Comp Freq', 'Time', 'ZS Power', 'Image Time', 
@@ -281,16 +282,27 @@ class DataGraph(wx.Frame) :
             'Evap 1 Comp Z', 'Pumping Freq', 'Comp X Earth', 'Comp Y Earth', 'Comp Z Earth',
             'Evap Time 6', 'Comp Z 2'])
 
-        x1 = [0, 1, 2, 3]
-        y1 = [0, 3, 4, 1]
+        widgets.ToggleButton(
+            value=False,
+            description='Legend On?',
+            disabled=False
+        )
+
+        xSelection = self.f
+        ySelection = self.g
+
+        x1 = self.ParseFile(xSelection)
+        y1 = self.ParseFile(ySelection)
+        print(self.ParseFile(xSelection))
+        print(self.ParseFile(ySelection))
 
         ##varGraph = wxmplot.interactive.PlotPanel(self, (1750, 1600))
         ## here have a series of ifs that will check the current variables selected
 
         plt.plot(x1, y1, 'o', color ='black')
-        plt.xlabel("x variable")
-        plt.ylabel("Y Variable")
-        plt.title("X vs. Y Graph")
+        plt.xlabel(xSelection)
+        plt.ylabel(ySelection)
+        plt.title(xSelection + " vs. " + ySelection + " Graph")
         ##plt.plot(x2, y2, 'o', color = "red")
 
         self.Show()
