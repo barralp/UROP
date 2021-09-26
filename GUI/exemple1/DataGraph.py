9577d23608041bbe218c3f67bfae4e3a89f690d5
Julia-Pockat <julia.pockat@gmail.com>
2021-09-26 17:51:51 -0400

    got rid of old data graph that did not work, tried to figure out how to fix size mismatch.

--- GUI/exemple1/DataGraph.py
+++ GUI/exemple1/DataGraph.py
@@ -1,33 +1,44 @@
 import sys
 sys.path.insert(0, '../../database')
 from communicate_database import getAMDuration, getAMFreq, getASPower, getASPowermW, getASPowermW2, getBigZ, getBlinckingDuration, getBlinckingFreq, getBlinckingLength, getBlinckingTime, getCameraFudgeTime, getCompHoldTime, getCompLevel, getCompLevel2, getCompTime, getCompTime2, getCompX, getCompXEarth, getCompY, getCompYEarth, getCompZ, getCompZ2, getCompZEarth, getDopplerCoolFreq, getDummy, getEvap1CompZ, getEvap1EndGradient, getEvap2Factor, getEvap5BigZ, getEvapEndGradient, getEvapGradient1, getEvapGradient2, getEvapGradient3, getEvapGradientInit, getEvapScan, getEvapTime1, getEvapTime2, getEvapTime3, getEvapTime4, getEvapTime5, getEvapTime6, getFeshbachCurrent, getFinalBField, getFinalODT1, getFinalYComp, getFreq1, getFreq2, getFreq3, getFreq4, getFreq5, getFreqCompTime2, getImageTime, getImgFreq, getInTrapCoolFreq, getInTrapCoolTime, getIodineFreq, getIterationCount, getIterationNum, getLevel1, getLevel2, getLevel3, getLevel4, getLevel5, getLoadCurrent, getLoadTime, getLossTime, getMOTCompFreq, getMOTCompFreq2, getMOTCurrentAmps, getMOTLevel, getMOTLoadCurrentAmps, getMolassesLevel, getMotLoadFreq, getODT1Evap1End, getODT1Final, getODT1Init, getODT2Evap1End, getODT2Final, getODT2Init, getODTCompX, getODTCompY, getODTCompZ, getODTHoldTime, getODTLoad_MOTFreq, getODTLoad_MOT_Freq, getODTMolasses, getODTMolassesZField, getODTRamp, getODTRampUp, getODTTOFBigZ, getODTTOFGradient, getPumpTime, getPumpingFreq, getRunID, getRunningCounter, getSGOn, getSGOn2, getTCFreq, getTOF, getTau, getTime, getTimeStamp, getTotalExp, getVar60, getWaitTime, getWee, getZSPower
+import matplotlib
+import numpy
 import wx
 import numpy as np
-import matplotlib.pyplot as plt
-from ipywidgets import interact, interactive, fixed, interact_manual
-import ipywidgets as widgets
+matplotlib.use('WXAgg')
+
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
 from matplotlib.figure import Figure
-import wx.lib.scrolledpanel
 
-class DataGraph(wx.StaticBoxSizer) :
-    def __init__(self, parent, label = 'Database'):
-        self.app = wx.App()
-        databaseBox = wx.StaticBox(label = label)
-        super(DataGraph, self).__init__(databaseBox, wx.VERTICAL)
-        self.init_databaseBox_databaseBox()
-    
-    def f(x) :
-        global varX
-        varX = x
-        return x
 
-    def g(y) :
-        global varY
-        varY = y
-        return y
+class PlotPanel( wx.Panel ) :
+    def __init__( self, parent, position, xVar, yVar ) :
+        wx.Panel.__init__( self, parent, pos=position, size=(800,320) )
+        # initialize matplotlib 
+        self.figure = matplotlib.figure.Figure( None, facecolor="white" )
+        self.canvas = matplotlib.backends.backend_wxagg.FigureCanvasWxAgg( self, -1, self.figure )
+        self.axes = self.figure.add_subplot(111)
+        self.axes.grid(True, color="gray")
+        self.axes.set_xbound( (0,5) )
+        self.axes.set_ybound( (3,80) )
+        self.axes.set_xlabel( "X Var" ) ## change later
+        self.axes.set_ylabel( "Y Var" )
+        self.axes.grid(True, color="gray")
+        self._SetSize()
+        self.Bind( wx.EVT_SIZE, self._SetSize )
+        #self.XData = self.getX()
+        #self.YData = self.getY()
+    #-----------------------------------------------------------------------------------
+    def _SetSize( self, event=None ):
+        pixels = self.GetSize() 
+        self.SetSize( pixels )
+        self.canvas.SetSize( pixels )
 
-    def parseFile(self, selection) :
+        dpi = self.figure.get_dpi()
+        self.figure.set_size_inches( float( pixels[0] ) / dpi,float( pixels[1] ) / dpi )
+    #------------------------------------------------------------------------------------
+
+    def changeVar(self, selection) :
         data = []
         if (selection == 'Run ID') :
             data = getRunID()
@@ -248,92 +259,83 @@ class DataGraph(wx.StaticBoxSizer) :
             print('no matching')
         return data
 
-        
-    def makeGraph(self) :
-        # interact(self.f, x=['Run ID', 'Iteration Num', 'Iteration Count', 'Running Counter',
-        #     'TOF', 'Comp Level', 'Img Freq', 'Dummy', 'Iodine Freq', 'Final B Field',
-        #     'Camera Fudge Time', 'Load Time', 'Comp Time', 'Load Current', 'Time Stamp', 
-        #     'Wee', 'Mot Load Freq', 'Mot Comp Freq', 'Time', 'ZS Power', 'Image Time', 
-        #     'MOT Level', 'Comp X', 'Comp Y', 'Loss Time', 'TC Freq', 'Comp Z', 'MOT Current Amps',
-        #     'MOT Load Current Amps', 'Comp Time 2', 'Comp Level 2', 'MOT Comp Freq 2', 'Freq Comp Time 2',
-        #     'Final Y Comp', 'Comp Hold Time', 'Wait Time', 'AS Power', 'AS Power mW', 'AS Power mW 2', 
-        #     'Level 1', 'Level 2', 'Level 3', 'Level 4', 'Level 5', 'Freq 1', 'Freq 2',
-        #     'Freq 3', 'Freq 4', 'Freq 5', 'ODT Ramp', 'ODT Hold Time', 'ODT 1 Final', 'ODT 2 Final', 
-        #     'Evap Time 2', 'Evap Time 1', 'Evap Time 3', 'Big Z', 'SG On', 'SG On 2', 'Pump Time',
-        #     'Doppler Cool Freq', 'ODT 1 Init', 'ODT 1 Evap 1 End', 'ODT 2 Init', 'ODT 2 Evap 1 End',
-        #     'Feshbach Current', 'Evap Time 4', 'AM Freq', 'AM Duration', 'ODT Ramp Up', 'Evap 2 Factor',
-        #     'Tau', 'Total Exp', 'In Trap Cool Freq', 'In Trap Cool Time', 'Evap Time 5', 'Evap 1 End Gradient',
-        #     'Var 60', 'Evap Scan', 'Evap Gradient Init', 'Evap Gradient 1', 'Evap Gradient 2',
-        #     'Evap Gradient 3', 'Evap End Gradient', 'ODT TOF Gradient', 'ODT TOF Big Z', 
-        #     'Evap 5 Big Z', 'Final ODT 1', 'Blincking Time', 'Blincking Freq', 'Molasses Level',
-        #     'Blincking Duration', 'ODT Molasses', 'ODT Molasses Z Field', 'ODT Load MOT Freq',
-        #     'ODT Comp X', 'ODT Comp Y', 'ODT Comp Z', 'Blincking Length', 'ODT Load MOT Freq', 
-        #     'Evap 1 Comp Z', 'Pumping Freq', 'Comp X Earth', 'Comp Y Earth', 'Comp Z Earth',
-        #     'Evap Time 6', 'Comp Z 2'])
-        # interact(self.g, y=['Run ID', 'Iteration Num', 'Iteration Count', 'Running Counter', 
-        #     'TOF', 'Comp Level', 'Img Freq', 'Dummy', 'Iodine Freq', 'Final B Field',
-        #     'Camera Fudge Time', 'Load Time', 'Comp Time', 'Load Current', 'Time Stamp',
-        #     'Wee', 'Mot Load Freq', 'Mot Comp Freq', 'Time', 'ZS Power', 'Image Time', 
-        #     'MOT Level', 'Comp X', 'Comp Y', 'Loss Time', 'TC Freq', 'Comp Z', 'MOT Current Amps',
-        #     'MOT Load Current Amps', 'Comp Time 2', 'Comp Level 2', 'MOT Comp Freq 2', 'Freq Comp Time 2',
-        #     'Final Y Comp', 'Comp Hold Time', 'Wait Time', 'AS Power', 'AS Power mW', 'AS Power mW 2',
-        #     'MOT Current 2', 'Level 1', 'Level 2', 'Level 3', 'Level 4', 'Level 5', 'Freq 1', 'Freq 2',
-        #     'Freq 3', 'Freq 4', 'Freq 5', 'ODT Ramp', 'ODT Hold Time', 'ODT 1 Final', 'ODT 2 Final',
-        #     'Evap Time 2', 'Evap Time 1', 'Evap Time 3', 'Big Z', 'SG On', 'SG On 2', 'Pump Time', 
-        #     'Doppler Cool Freq', 'ODT 1 Init', 'ODT 1 Evap 1 End', 'ODT 2 Init', 'ODT 2 Evap 1 End',
-        #     'Feshbach Current', 'Evap Time 4', 'AM Freq', 'AM Duration', 'ODT Ramp Up', 'Evap 2 Factor',
-        #     'Tau', 'Total Exp', 'In Trap Cool Freq', 'In Trap Cool Time', 'Evap Time 5', 'Evap 1 End Gradient',
-        #     'Var 60', 'Evap Scan', 'Evap Gradient Init', 'Evap Gradient 1', 'Evap Gradient 2',
-        #     'Evap Gradient 3', 'Evap End Gradient', 'ODT TOF Gradient', 'ODT TOF Big Z', 
-        #     'Evap 5 Big Z', 'Final ODT 1', 'Blincking Time', 'Blincking Freq', 'Molasses Level',
-        #     'Blincking Duration', 'ODT Molasses', 'ODT Molasses Z Field', 'ODT Load MOT Freq',
-        #     'ODT Comp X', 'ODT Comp Y', 'ODT Comp Z', 'Blincking Length', 'ODT Load MOT Freq', 
-        #     'Evap 1 Comp Z', 'Pumping Freq', 'Comp X Earth', 'Comp Y Earth', 'Comp Z Earth',
-        #     'Evap Time 6', 'Comp Z 2'])
-        # widgets.ToggleButton(
-        #     value=False,
-        #     description='Legend On?',
-        #     disabled=False
-        # )
-
-        # widgets.ToggleButton(
-        #     value=False,
-        #     description='Export Graph',
-        #     disabled=False
-        # )
-
-        # widgets.ToggleButton(
-        #     value=False,
-        #     description='On/Off',
-        #     disabled=False
-        # )
-
-        # xSelection = varY
-        # ySelection = varX
-
-        # x1 = self.parseFile(xSelection)
-        # y1 = self.parseFile(ySelection)
-        # print(self.parseFile(xSelection))
-        # print(self.parseFile(ySelection))
-
-        ##varGraph = wxmplot.interactive.PlotPanel(self, (1750, 1600))
-        ## here have a series of ifs that will check the current variables selected
-
-        # plt.plot(x1, y1, 'o', color ='black')
-        # plt.xlabel(xSelection)
-        # plt.ylabel(ySelection)
-        #plt.title(xSelection + ' vs. ' + ySelection + " Graph")
-        ##plt.plot(x2, y2, 'o', color = "red")
-        self.panel = wx.lib.scrolledpanel.ScrolledPanel(self, id = -1, size = (1,1))
-        self.figureImage = Figure(figsize = (8,8))
-        self.axes1 = self.figureImage.add_subplot(111)
-        self.canvasImage = FigureCanvas(self.panel, -1, self.figureImage)
-        self.Add(self.canvasImage, flag=wx.ALL|wx.SHAPED, border=5)
-        self.axes1.plot([0, 1, 2], [0, 1, 4])
-        self.canvasImage.draw()
+def returnFuncVar(self) :
+    return 0
+    
+class MainWindow(wx.Frame):
+    def __init__(self, parent):
+        self.app = wx.App()
+        wx.Frame.__init__(self, parent, title="Database Variable Graphing", size=(1000,600))
+        variables = ['Run ID', 'Iteration Num', 'Iteration Count', 'Running Counter', 
+            'TOF', 'Comp Level', 'Img Freq', 'Dummy', 'Iodine Freq', 'Final B Field',
+            'Camera Fudge Time', 'Load Time', 'Comp Time', 'Load Current', 'Time Stamp',
+            'Wee', 'Mot Load Freq', 'Mot Comp Freq', 'Time', 'ZS Power', 'Image Time', 
+            'MOT Level', 'Comp X', 'Comp Y', 'Loss Time', 'TC Freq', 'Comp Z', 'MOT Current Amps',
+            'MOT Load Current Amps', 'Comp Time 2', 'Comp Level 2', 'MOT Comp Freq 2', 'Freq Comp Time 2',
+            'Final Y Comp', 'Comp Hold Time', 'Wait Time', 'AS Power', 'AS Power mW', 'AS Power mW 2',
+            'MOT Current 2', 'Level 1', 'Level 2', 'Level 3', 'Level 4', 'Level 5', 'Freq 1', 'Freq 2',
+            'Freq 3', 'Freq 4', 'Freq 5', 'ODT Ramp', 'ODT Hold Time', 'ODT 1 Final', 'ODT 2 Final',
+            'Evap Time 2', 'Evap Time 1', 'Evap Time 3', 'Big Z', 'SG On', 'SG On 2', 'Pump Time', 
+            'Doppler Cool Freq', 'ODT 1 Init', 'ODT 1 Evap 1 End', 'ODT 2 Init', 'ODT 2 Evap 1 End',
+            'Feshbach Current', 'Evap Time 4', 'AM Freq', 'AM Duration', 'ODT Ramp Up', 'Evap 2 Factor',
+            'Tau', 'Total Exp', 'In Trap Cool Freq', 'In Trap Cool Time', 'Evap Time 5', 'Evap 1 End Gradient',
+            'Var 60', 'Evap Scan', 'Evap Gradient Init', 'Evap Gradient 1', 'Evap Gradient 2',
+            'Evap Gradient 3', 'Evap End Gradient', 'ODT TOF Gradient', 'ODT TOF Big Z', 
+            'Evap 5 Big Z', 'Final ODT 1', 'Blincking Time', 'Blincking Freq', 'Molasses Level',
+            'Blincking Duration', 'ODT Molasses', 'ODT Molasses Z Field', 'ODT Load MOT Freq',
+            'ODT Comp X', 'ODT Comp Y', 'ODT Comp Z', 'Blincking Length', 'ODT Load MOT Freq', 
+            'Evap 1 Comp Z', 'Pumping Freq', 'Comp X Earth', 'Comp Y Earth', 'Comp Z Earth',
+            'Evap Time 6', 'Comp Z 2']
+        relationsX = ["x", "log(x)", "x^2", "x^0.5"]
+        relationsY = ["y", "log(y)", "y^2", "y^0.5"]
+        self.textX = wx.StaticText(self, label = "X Variable", pos = (20, 380))
+        self.textY = wx.StaticText(self, label = "Y Variable", pos = (20, 405))  
+        self.dropDownX = wx.ComboBox(self, choices = variables, pos = (80, 375))
+        self.dropDownY = wx.ComboBox(self, choices = variables, pos = (80, 400))
+        self.dropDownXRelations = wx.ComboBox(self, choices = relationsX, pos = (300, 375))
+        self.dropDownYRelations = wx.ComboBox(self, choices = relationsY, pos = (300, 400))
+        selectionX = self.dropDownX
+        selectionY = self.dropDownY
+        self.dropDownX.Bind(wx.EVT_COMBOBOX, self.updateDropDown)
+        self.dropDownY.Bind(wx.EVT_COMBOBOX, self.updateDropDown)
+        #self.spin = wx.SpinCtrl(self.panel)
+        #self.button = wx.Button(self.panel, label="Update")
+        #self.stop   = wx.Button(self.panel, label="Stop")
+        self.panel = wx.Panel(self)
+        self.sizer = wx.BoxSizer()
+        #self.sizer.Add(self.button)
+        #self.sizer.Add(self.stop)
+        if selectionX.GetStringSelection() != "" and selectionY.GetStringSelection() != "":
+            self.graph = PlotPanel( self, position=(20, 50), xVar=selectionX.GetStringSelection(), 
+                yVar=selectionY.GetStringSelection() )
+        else:
+            self.graph = PlotPanel( self, position=(20, 50), xVar=[], yVar=[])
+        self.panel.SetSizerAndFit(self.sizer)
+        self.updateDropDown(0)
         self.Show()
 
-if __name__ == '__main__':
-    graph = DataGraph(None)
-    graph.makeGraph()
-    graph.app.MainLoop()
+        # Use EVT_CHAR_HOOK on Frame insted of wx.EVT_KEY_UP on SpinCtrl
+        # to disable "on Enter go to next widget" functionality
+        #self.Bind(wx.EVT_CHAR_HOOK, self.OnKey) 
+        #self.button.Bind(wx.EVT_BUTTON, self.OnUpdate)
+        #self.stop.Bind(wx.EVT_BUTTON, self.OnStop)
+
+    def updateDropDown(self, event) :       
+        self.graph.axes.set_title(self.dropDownX.GetStringSelection()
+            + " vs. " + self.dropDownY.GetStringSelection()) 
+        self.graph.axes.set_xlabel(self.dropDownX.GetStringSelection()) ## change later
+        self.graph.axes.set_ylabel(self.dropDownY.GetStringSelection())
+        #self.graph.axes.set_x(self.graph.changeVar(self.dropDownX.GetStringSelection()))
+        #self.graph.axes.set_y(self.graph.changeVar(self.dropDownY.GetStringSelection()))
+        self.graph.axes.plot(self.graph.changeVar(self.dropDownX.GetStringSelection()),
+            self.graph.changeVar(self.dropDownY.GetStringSelection()))
+        self.graph.canvas.draw()
+        print(getAMDuration())
+        print(getAMFreq())
+
+
+
+
+app = wx.App(False)
+win = MainWindow(None)
+app.MainLoop()
