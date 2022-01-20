e6d1e448055c3869ac248f1da5ea8c056570164b
Julia-Pockat <julia.pockat@gmail.com>
2022-01-20 12:11:34 -0500

    Added box sizers

--- GUI/exemple1/DataGraph.py
+++ GUI/exemple1/DataGraph.py
@@ -20,10 +20,16 @@ from matplotlib.figure import Figure
 
 
 class PlotPanel( wx.Panel ) :
-    def __init__( self, parent, position) :
-        wx.Panel.__init__( self, parent, pos=position, size=(625,250) )
+    def __init__( self, parent):#, position) :
+        wx.Panel.__init__( self, parent, size=(625, 350))#, pos=position)
         # initialize matplotlib 
-        self.figure = matplotlib.figure.Figure( None, facecolor="white" )
+        self.figure = matplotlib.figure.Figure(facecolor="white", figsize=(6,6))
+        
+        self.mainBoxSizer = wx.BoxSizer(wx.HORIZONTAL)
+        
+        self.figureBox = wx.StaticBox(self, label='Graph box')
+        self.figureBoxSizer = wx.StaticBoxSizer(self.figureBox, wx.HORIZONTAL)
+
         self.canvas = matplotlib.backends.backend_wxagg.FigureCanvasWxAgg( self, -1, self.figure )
         self.axes = self.figure.add_subplot(111)
         self.axes.grid(True, color="gray")
@@ -32,6 +38,12 @@ class PlotPanel( wx.Panel ) :
         self.axes.set_xlabel( "X Var" ) ## change later
         self.axes.set_ylabel( "Y Var" )
         self.axes.grid(True, color="gray")
+
+        self.figureBoxSizer.Add(self.canvas, flag=wx.ALL, border=5)
+
+        self.menuBox = wx.StaticBox(self, label='Menu')
+        self.menuBoxSizer = wx.StaticBoxSizer(self.menuBox, wx.HORIZONTAL)
+
         self._SetSize()
         self.Bind( wx.EVT_SIZE, self._SetSize )
         variables = ['runID', 'IterationNum', 'IterationCount', 'RunningCounter', 
@@ -57,53 +69,69 @@ class PlotPanel( wx.Panel ) :
         relationsX = ["x", "log(x)", "x^2", "x^0.5"]
         relationsY = ["y", "log(y)", "y^2", "y^0.5"]
 
-        self.textX = wx.StaticText(self, label = "X Variable", pos = (10, 200))
-        self.textY = wx.StaticText(self, label = "Y Variable", pos = (10, 620))
+        self.xBox = wx.StaticBox(self, label='X parameters')
+        self.xBoxSizer = wx.StaticBoxSizer(self.xBox, wx.VERTICAL)
+        self.yBox = wx.StaticBox(self, label='Y parameters')
+        self.yBoxSizer = wx.StaticBoxSizer(self.yBox, wx.VERTICAL)
 
-        self.dropDownX = wx.ComboBox(self, choices = variables, pos = (70, 590))
-        self.dropDownY = wx.ComboBox(self, choices = variables, pos = (70, 615))
+        self.textX1 = wx.StaticText(self, label = "X Variable")#, pos = (10, 285))
+        self.textY1 = wx.StaticText(self, label = "Y Variable")#, pos = (10, 310))  
+    
+        self.dropDownX1 = wx.ComboBox(self, choices = variables)#, pos = (70, 590))
+        self.dropDownY1 = wx.ComboBox(self, choices = variables)#, pos = (70, 615))
 
-        self.textXRelations = wx.StaticText(self, label = "X Transformation", pos = (240, 275))
-        self.textYRelations = wx.StaticText(self, label = "Y Transformation", pos = (240, 300))
+        self.textXRelations = wx.StaticText(self, label = "X Transformation")#, pos = (200, 285))
+        self.textYRelations = wx.StaticText(self, label = "Y Transformation")#, pos = (200, 310))
 
-        self.dropDownXRelations1 = wx.ComboBox(self, choices = relationsX, pos = (340, 270))
-        self.dropDownYRelations1 = wx.ComboBox(self, choices = relationsY, pos = (340, 295))
+        self.dropDownXRelations1 = wx.ComboBox(self, choices = relationsX)#, pos = (340, 270))
+        self.dropDownYRelations1 = wx.ComboBox(self, choices = relationsY)#, pos = (340, 295))
         
+        self.xBoxSizer.Add(self.textX1, flag=wx.ALL|wx.EXPAND, border = 5)
+        self.xBoxSizer.Add(self.dropDownX1, flag=wx.ALL|wx.EXPAND, border = 5)
+        self.xBoxSizer.Add(self.textXRelations, flag=wx.ALL|wx.EXPAND, border = 5)
+        self.xBoxSizer.Add(self.dropDownXRelations1, flag=wx.ALL|wx.EXPAND, border = 5)
+        self.yBoxSizer.Add(self.textY1, flag=wx.ALL|wx.EXPAND, border = 5)
+        self.yBoxSizer.Add(self.dropDownY1, flag=wx.ALL|wx.EXPAND, border = 5)
+        self.yBoxSizer.Add(self.textYRelations, flag=wx.ALL|wx.EXPAND, border = 5)
+        self.yBoxSizer.Add(self.dropDownYRelations1, flag=wx.ALL|wx.EXPAND, border = 5)
+
         #selectionX1 = self.dropDownX1
         #selectionY1 = self.dropDownY1
         
-        self.dropDownX.Bind(wx.EVT_COMBOBOX, self.updateDropDown)
-        self.dropDownY.Bind(wx.EVT_COMBOBOX, self.updateDropDown)
+        self.dropDownX1.Bind(wx.EVT_COMBOBOX, self.updateDropDown)
+        self.dropDownY1.Bind(wx.EVT_COMBOBOX, self.updateDropDown)
         #self.dropDownX2.Bind(wx.EVT_COMBOBOX, self.updateDropDown)
         #self.dropDownY2.Bind(wx.EVT_COMBOBOX, self.updateDropDown)
         
-        self.updateButton1 = wx.Button(self, label="Update Data", pos=(1000, 330))
-        self.boundBoxX = wx.StaticText(self, label="X bounds", pos=(660, 335))
+        #self.updateButton1 = wx.Button(self, label="Update Data")#, pos=(1000, 330))
+        #self.boundBoxX = wx.StaticText(self, label="X bounds")#, pos=(660, 335))
         #self.lowerBoundX = wx.text
-        self.boundBoxY = wx.StaticText(self, label="Y bounds", pos=(660, 360))
+        #self.boundBoxY = wx.StaticText(self, label="Y bounds")#, pos=(660, 360))
 
-        self.export = wx.Button(self, label="Stop")
-        self.panel = wx.Panel(self)
-        self.sizer = wx.BoxSizer()
-        self.panel.SetSizerAndFit(self.sizer)
+        #self.export = wx.Button(self, label="Stop")
+        ##self.panel = wx.Panel(self)
+        ##self.sizer = wx.BoxSizer()
+        ##self.panel.SetSizerAndFit(self.sizer)
         
-        self.exportDataButton1 = wx.Button(self, label="Copy Data", pos=(410, 295))
-        self.exportDataButton1.Bind(wx.EVT_BUTTON, lambda event: self.copyToClipboard(1))
-        self.exportDataButton2 = wx.Button(self, label="Copy Data", pos=(410, 615))
-        self.exportDataButton2.Bind(wx.EVT_BUTTON, lambda event: self.copyToClipboard(2))
+        #self.exportDataButton1 = wx.Button(self, label="Copy Data")#, pos=(410, 295))
+        #self.exportDataButton1.Bind(wx.EVT_BUTTON, lambda event: self.copyToClipboard(1))
+        #self.exportDataButton2 = wx.Button(self, label="Copy Data")#, pos=(410, 615))
+        #self.exportDataButton2.Bind(wx.EVT_BUTTON, lambda event: self.copyToClipboard(2))
 
-        self.doublePlot1 = wx.CheckBox(self, label="Display Two Plots", pos=(410, 270))
-        self.doublePlot1.Bind(event=wx.EVT_CHECKBOX, handler=self.toggleSecondPlot)
+        #self.doublePlot1 = wx.CheckBox(self, label="Display Two Plots")#, pos=(410, 285))
+        #self.doublePlot1.Bind(event=wx.EVT_CHECKBOX, handler=self.toggleSecondPlot)
 
         self.updateDropDown(0)
 
-        self.dropDownX = wx.ComboBox(self, choices = [], pos = (70, 270))
-        self.dropDownY = wx.ComboBox(self, choices = [], pos = (70, 295))
+        #self.dropDownX1 = wx.ComboBox(self, choices = variables)#, pos = (70, 270))
+        #self.dropDownY1 = wx.ComboBox(self, choices = variables)#, pos = (70, 295))
 
-        self.textX1 = wx.StaticText(self, label = "X Variable", pos = (10, 275))
-        self.textY1 = wx.StaticText(self, label = "Y Variable", pos = (10, 300))  
+        self.menuBoxSizer.Add(self.yBoxSizer, flag=wx.ALL|wx.EXPAND, border = 5)
+        self.menuBoxSizer.Add(self.xBoxSizer, flag=wx.ALL|wx.EXPAND, border = 5)
 
-        self.Show()
+        self.mainBoxSizer.Add(self.figureBoxSizer, flag=wx.ALL|wx.EXPAND, border = 5)
+
+        # self.Show()
     #-----------------------------------------------------------------------------------
 
     def applyTransformation(self, event) : # use for plotting different 
@@ -139,8 +167,8 @@ class PlotPanel( wx.Panel ) :
 
     def updateDropDown(self, event) :
         self.axes.cla()  
-        dropDownX = self.dropDownX.GetStringSelection()
-        dropDownY = self.dropDownY.GetStringSelection()
+        dropDownX = self.dropDownX1.GetStringSelection()
+        dropDownY = self.dropDownY1.GetStringSelection()
         self.axes.set_title(dropDownX + " vs. " + dropDownY) 
         self.axes.set_xlabel(dropDownX) ## change later
         self.axes.set_ylabel(dropDownY)
@@ -181,9 +209,38 @@ class MainWindow(wx.Frame):
     def __init__(self, parent):
         self.app = wx.App()
         wx.Frame.__init__(self, parent, title="Database Variable graphing", size=(500,300))
-        self.graph1 = PlotPanel(self, position=(10, 10))
-        self.graph2 = PlotPanel(self, position=(700, 10))
-        self.graph3 = PlotPanel(self, position=(10, 400))
+        self.mainWindowBoxSizer = wx.BoxSizer(wx.HORIZONTAL)
+
+        self.firstRowBox = wx.StaticBox(self, label='Upper box')
+        self.firstRowBoxSizer = wx.StaticBoxSizer(self.firstRowBox, wx.VERTICAL)
+
+        self.Box11 = wx.StaticBox(self, label='Graph 1')
+        self.BoxSizer11 = wx.StaticBoxSizer(self.Box11, wx.HORIZONTAL)
+        self.Box12 = wx.StaticBox(self, label='Graph 2')
+        self.BoxSizer12 = wx.StaticBoxSizer(self.Box12, wx.HORIZONTAL)
+        self.firstRowBoxSizer.Add(self.BoxSizer11, flag=wx.ALL|wx.EXPAND, border = 5)
+        self.firstRowBoxSizer.Add(self.BoxSizer12, flag=wx.ALL|wx.EXPAND, border = 5)
+
+        self.secondRowBox = wx.StaticBox(self, label='Lower box')
+        self.secondRowBoxSizer = wx.StaticBoxSizer(self.secondRowBox, wx.VERTICAL)
+
+        self.Box21 = wx.StaticBox(self, label='Graph 3')
+        self.BoxSizer21 = wx.StaticBoxSizer(self.Box21, wx.HORIZONTAL)
+        self.Box22 = wx.StaticBox(self, label='The rest')
+        self.BoxSizer22 = wx.StaticBoxSizer(self.Box22, wx.HORIZONTAL)
+        self.secondRowBoxSizer.Add(self.BoxSizer21, flag=wx.ALL|wx.EXPAND, border = 5)
+        self.secondRowBoxSizer.Add(self.BoxSizer22, flag=wx.ALL|wx.EXPAND, border = 5)
+
+        self.graph1 = PlotPanel(self)#, position=(10, 10))
+        self.BoxSizer11.Add(self.graph1, flag=wx.ALL|wx.EXPAND, border = 5)
+        self.graph2 = PlotPanel(self)#, position=(700, 10))
+        self.BoxSizer12.Add(self.graph2, flag=wx.ALL|wx.EXPAND, border = 5)
+        self.graph3 = PlotPanel(self)#, position=(10, 400))
+        self.BoxSizer21.Add(self.graph3, flag=wx.ALL|wx.EXPAND, border = 5)
+
+        self.mainWindowBoxSizer.Add(self.firstRowBoxSizer, flag=wx.ALL|wx.EXPAND, border = 5)
+        self.mainWindowBoxSizer.Add(self.secondRowBoxSizer, flag=wx.ALL|wx.EXPAND, border = 5)
+
         self.Show()
 
 app = wx.App(False)
